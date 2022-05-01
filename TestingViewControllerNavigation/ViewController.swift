12630ad7e2d51e70606f3683802d60b065044761
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-05-01 20:28:54 +0900

    Push UIViewController into UINavigationController. Wait for expectations.

--- TestingViewControllerNavigation/ViewController.swift
+++ TestingViewControllerNavigation/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nextViewButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func nextViewButtonTapped(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let secondViewController = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        
+        navigationController?.pushViewController(secondViewController, animated: true)
+    }
+    
 
 }
 
--- TestingViewControllerNavigationTests/TestingViewControllerNavigationTests.swift
+++ TestingViewControllerNavigationTests/TestingViewControllerNavigationTests.swift
@@ -9,28 +9,42 @@ import XCTest
 @testable import TestingViewControllerNavigation
 
 class TestingViewControllerNavigationTests: XCTestCase {
+    
+    var sut: ViewController!
+    var navigationController: UINavigationController!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        sut = storyboard.instantiateViewController(withIdentifier: "ViewController") as? ViewController
+        sut.loadViewIfNeeded()
+        
+        navigationController = UINavigationController(rootViewController: sut)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        navigationController = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testNextViewButton_WhenTapped_SecondViewControllerIsPushed() {
+        let myPredicate = NSPredicate { input, _ in
+            return (input as? UINavigationController)?.topViewController is SecondViewController
         }
+        
+        expectation(for: myPredicate, evaluatedWith: navigationController)
+        
+        sut.nextViewButton.sendActions(for: .touchUpInside)
+        
+        waitForExpectations(timeout: 1.5) // 遷移時のanimation時間を考慮, 何回かテストしてみて時間が十分か確認
+        
+        
+        
+        // 以下の方法では、テストできない。（buttonをtapしてから遷移するまでのanimation時間を考慮していないため）
+//        guard let _ = navigationController.topViewController as? SecondViewController else {
+//            XCTFail()
+//            return
+//        }
     }
 
 }
